25d444c6d3e6c8e44bf6934345e595a255cb8485
Vladimir Ustyanivsky <bender1102@gmail.com>
2023-07-20 20:02:36 +0300

    new additions

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -2,6 +2,8 @@ import time
 from behave import step
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import requests
+import json
 
 
 def open(context, *args):
@@ -11,7 +13,7 @@ def open(context, *args):
 @step('Enter site "{url}"')
 def step_impl(context, url):
     context.driver.get('http://' + url)
-    time.sleep(3)
+    time.sleep(1)
 
 
 @step('Find area with email "{email}" and click button "{value}"')
@@ -19,3 +21,22 @@ def step_impl(context, email, value):
     element = context.driver.find_element(By.XPATH, f'//tr/td[contains(text(), "{email}")]/..//a[text()="{value}"]')
     element.click()
     time.sleep(5)
+
+
+@step('Register new user with "{email}" email, "{name}" name, "{password}" password')
+def step_impl(context, email, name, password):
+    date = """
+    {
+    "email": "email",
+    "name": "name",
+    "password": "password"
+    }
+    """
+    body = json.loads(date)
+    body['email'] = email
+    body['name'] = name
+    body['password'] = password
+
+    response = requests.post('http://users.bugred.ru/tasks/rest/doregister', body)
+    assert response.status_code == 200
+
--- features/test/test.feature
+++ features/test/test.feature
@@ -2,5 +2,6 @@
 Feature: Users Bugred
 
   Scenario: Click Lookup button
-    Given Enter site "users.bugred.ru"
-    Then Find area with email "Turner_Dibbert@gmail.com" and click button "Посмотреть"
+#    Given Enter site "users.bugred.ru"
+#    Then Find area with email "6@mail.ru" and click button "Посмотреть"
+    Given Register new user with "tety-testy-test@mail.com" email, "Bender B Rodriguez" name, "1" password
